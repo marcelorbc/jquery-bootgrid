1fbe0aa86c949f8cd14800727d799d8c9c76eae3
DESKTOP-88SLJSG\sunma <sun.marcelo@gmail.com>
2016-10-24 19:41:06 -0200

    da

--- dist/jquery.bootgrid.da.js
+++ dist/jquery.bootgrid.da.js
@@ -1,5 +1,5 @@
 /*! 
- * jQuery Bootgrid DA v1.3.2 - 10/24/2016
+ * jQuery Bootgrid DA v1.3.4 - 10/24/2016
  * Copyright (c) 2014-2016 Rafael Staib (http://www.jquery-bootgrid.com)
  * Licensed under MIT http://www.opensource.org/licenses/MIT
  */
@@ -1129,10 +1129,10 @@
              *
              * @property method
              * @type String
-             * @default "POST"
+             * @default "GET"
              * @for ajaxSettings
              **/
-            method: "POST"
+            method: "GET"
         },
 
         /**
